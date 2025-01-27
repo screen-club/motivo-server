69a34235b7306d8790dd7702de894245d4fd6fc5
Yassin Siouda <yassin@siouda.com>
2025-01-27 17:10:55 +0000

    chore: wip cuda lib deps version finetuning

--- vibe/predict.py
+++ vibe/predict.py
@@ -4,6 +4,7 @@
 from cog import BasePredictor, Input, Path
 import os
 import time
+import subprocess
 
 
 class Predictor(BasePredictor):
@@ -17,12 +18,39 @@ class Predictor(BasePredictor):
         scale: float = Input(
             description="Factor to scale image by", ge=0, le=10, default=1.5
         ),
-    ) -> Path:
+    ) -> str:
         """Run a single prediction on the model"""
-
+        import torch
+        print(torch.__version__)
+        print(torch.version.cuda)
+        #return ""
         # execute command python demo.py --vid_file sample_video.mp4 --output_folder output/ --display
-        #time.sleep(500)
-        os.system(f"python VIBE/demo.py --vid_file {image} --output_folder output/ --display")
+       #time.sleep(500)
+        command = f"python ./scripts/demo.py --vid_file {image} --output_folder output/"
+        process = subprocess.Popen(
+            command,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            universal_newlines=True
+        )
+        # Print output in real-time
+        while True:
+            output = process.stdout.readline()
+            error = process.stderr.readline()
+            
+            if output:
+                print(output.strip())
+            if error:
+                print(error.strip())
+                
+            # Check if process has finished
+            if output == '' and error == '' and process.poll() is not None:
+                break
+        
+        return_code = process.poll()
+        return return_code
+
         # processed_input = preprocess(image)
         # output = self.model(processed_image, scale)
         # return postprocess(output)
